905e94314a93d8d985286243942ba2b4eae67da7
PeekabeeDev <devrandomizer3000@gmail.com>
2020-10-12 12:38:27 +0300

    initial

---
+++ SwiftPractice/SwiftPractice/SwiftPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPracticeApp.swift
+//  SwiftPractice
+//
+//  Created by User on 12.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
